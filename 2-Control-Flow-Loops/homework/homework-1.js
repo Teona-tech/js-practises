b49f1ce12b4f940b0e11217dde792945c4d963f6
teona <teoforchxidze30@gmail.com>
2020-11-15 21:46:52 +0400

    added loop homework

---
+++ 2-Control-Flow-Loops/homework/homework-1.js
@@ -0,0 +1,7 @@
+let sia=[2, 5, 9, 15, 0, 4];
+
+for (i of sia) {
+  if (i>3 && i<10){
+    console.log(i);
+  }
+}
---
+++ 2-Control-Flow-Loops/homework/homework-2.js
@@ -0,0 +1,10 @@
+var n = 1000;
+var num=0;
+
+while (n/2 > 50) {
+  n=n/2;
+  num++;
+}
+console.log(n); 
+
+console.log(num);
